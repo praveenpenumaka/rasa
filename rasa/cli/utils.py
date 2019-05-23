ccff567fe15f1cf26e9f9b99301851377de70eb4
Tanja Bergmann <tabergma@gmail.com>
2019-05-23 15:53:17 +0200

    Check sanity when loading domain.

--- rasa/cli/utils.py
+++ rasa/cli/utils.py
@@ -192,11 +192,10 @@ def signal_handler(sig, frame):
 
 
 def validate_domain(domain_path: Text):
-    from rasa.core.domain import Domain, check_domain_sanity, InvalidDomain
+    from rasa.core.domain import Domain, InvalidDomain
 
     try:
-        domain = Domain.load(domain_path)
-        check_domain_sanity(domain)
+        Domain.load(domain_path)
     except InvalidDomain as e:
         print_error("The provided domain file could not be loaded. Error: {}".format(e))
         sys.exit(1)
